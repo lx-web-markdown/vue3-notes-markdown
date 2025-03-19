929224c150f498119e9332119ad81687515740c3
linxiang <lionsom.linx@qq.com>
2025-03-19 11:32:29 +0800

    新增icon库

--- src/main.ts
+++ src/main.ts
@@ -10,8 +10,15 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 // 国际化
 import myI18n from './language'
+// 如果您正在使用CDN引入，请删除下面一行。
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
+
+// 注册element-plus的图标
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component)
+}
 
 app.use(createPinia())
 app.use(router)
