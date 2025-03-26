02f79b24190e14f28d26924ac945800725084224
linxiang <lionsom.linx@qq.com>
2025-03-26 14:22:07 +0800

    构建Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { fileURLToPath, URL } from 'node:url'
+import path from 'path'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
@@ -15,7 +16,14 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@vue-office/excel': path.resolve(__dirname, 'node_modules/@vue-office/excel/'),
+      '@vue-office/docx': path.resolve(__dirname, 'node_modules/@vue-office/docx/'),
+      '@vue-office/pdf': path.resolve(__dirname, 'node_modules/@vue-office/pdf/'),
+      '@vue-office/pptx': path.resolve(__dirname, 'node_modules/@vue-office/pptx/'),
     },
+  },
+  optimizeDeps: {
+    include: ['@vue-office/docx', '@vue-office/excel', '@vue-office/pdf', '@vue-office/pptx']
   },
 })
